5562c9a68a0b0f838ce35375d523687ccf7f9bf2
Kay Markschies <kayschima@web.de>
2020-06-20 11:49:25 +0200

    add is_admin field to user model; add command to create admin user via php artisan

---
+++ app/Console/Commands/AddAdminCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class AddAdminCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'shmc:addadmin {name} {email} {password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'add a admin user to the sports hall management calendar';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $user = new User;
+
+        $user->name = $this->argument('name');
+        $user->email = $this->argument('email');
+        $user->email_verified_at = now();
+        $user->is_admin = true;
+        $user->password = bcrypt($this->argument('password'));
+        $user->remember_token = Str::random(10);
+
+        $user->save();
+    }
+}
